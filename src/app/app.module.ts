da81ce1d9aaae49eadeab56145ed397935fc999b
yessine mkadem <yessinemkadem@yahoo.fr>
2022-09-26 10:14:46 +0200

    last change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { ExperienceComponent } from './experience/experience.component';
-import { EducationComponent } from './education/education.component';
-import { SkillsComponent } from './skills/skills.component';
-import { InterestComponent } from './interest/interest.component';
-import { AwardsComponent } from './awards/awards.component';
-
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
-    ContactComponent,
-    ExperienceComponent,
-    EducationComponent,
-    SkillsComponent,
-    InterestComponent,
-    AwardsComponent
   ],
   imports: [
     BrowserModule,
