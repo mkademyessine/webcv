417fe0520153120d9185075d857ac01ff929d4a5
yessine mkadem <yessinemkadem@yahoo.fr>
2022-09-26 09:42:50 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'webcv';
+  /*constructor(private el: ElementRef, private renderer:Renderer2){}
+
+  ngAfterViewInit(){
+
+this.renderer.setStyle(this.el.nativeElement.ownerDocument.body,'background', 'url(../assets/img/photo-02.jpg)');
+this.renderer.setStyle(this.el.nativeElement.ownerDocument.body,'background-position', 'center');
+
+}*/
+
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { InterestComponent } from './interest/interest.component';
+import { AwardsComponent } from './awards/awards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    InterestComponent,
+    AwardsComponent
   ],
   imports: [
     BrowserModule,
